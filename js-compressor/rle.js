4c636725702acd70c7c995a037cd49c0b22ffe63
anonym-266 <desmondtardzenyuy@gmail.com>
2025-04-12 05:39:41 +0100

    added decompress rle function

--- js-compressor/rle.js
+++ js-compressor/rle.js
@@ -1,21 +1,34 @@
-function compress_rle(data) {
-
+function compressRle(data) {
   let compressRleString = "";
   let dataLength = data.length;
   let i = 0;
 
-  while(i < dataLength) {
-
+  while (i < dataLength) {
     let counter = 1;
 
     while (i + 1 < dataLength && data[i] === data[i + 1]) {
       counter++;
-      i++
+      i++;
     }
     compressRleString += `${counter}${data[i]}`;
-    i++
+    i++;
   }
   return compressRleString;
 }
 
-console.log(compress_rle("this is a string"));
+function decompressRle(data) {
+  let i = 0;
+  let decompressRleString = "";
+  while (i < data.length) {
+    let tempCounter =  0;
+    let tempBuffer = "";
+
+    while (tempCounter < parseInt(data[i])) {
+      tempBuffer += data[i + 1];
+      tempCounter++
+    }
+    decompressRleString += tempBuffer;
+    i += 2;
+  }
+  return decompressRleString;
+}
