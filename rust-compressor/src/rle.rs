e1c1b8175d9800269ff75a13bad7f478577ca79b
anonym-266 <desmondtardzenyuy@gmail.com>
2025-04-14 05:42:28 +0100

    added rust compress rle function

--- rust-compressor/src/rle.rs
+++ rust-compressor/src/rle.rs
@@ -1,3 +1,27 @@
+use std::error::Error;
+
+fn compress(data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+  let mut compress_rle_string: Vec<u8> = Vec::new();
+  let mut i: usize =  0;
+
+  while i < data.len() {
+    let mut counter = 1;
+
+    while 
+      i + counter < data.len() &&
+      data[i] == data[i + counter] &&
+      counter < 255
+     {
+      counter += 1;
+    }
+    compress_rle_string.push(counter as u8);
+    compress_rle_string.push(data[i]);
+    i += counter;
+  }
+  Ok(compress_rle_string)
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
