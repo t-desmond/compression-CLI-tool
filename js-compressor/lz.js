9e92b8d41f5418f0351f4c19daa4dee91aead228
anonym-266 <desmondtardzenyuy@gmail.com>
2025-04-20 18:35:52 +0100

    added js decompress lz77 function

--- js-compressor/lz.js
+++ js-compressor/lz.js
@@ -11,7 +11,10 @@ function compress(data) {
     const searchBufferStart = Math.max(0, codingPosition - searchBufferSize);
     const searchBuffer = data.slice(searchBufferStart, codingPosition);
 
-    const lookaheadBufferEnd = Math.min(data.length, codingPosition + lookaheadBufferSize);
+    const lookaheadBufferEnd = Math.min(
+      data.length,
+      codingPosition + lookaheadBufferSize
+    );
     const lookaheadBuffer = data.slice(codingPosition, lookaheadBufferEnd);
 
     for (let i = lookaheadBuffer.length; i > 0; i--) {
@@ -21,12 +24,16 @@ function compress(data) {
       if (index !== -1) {
         offset = codingPosition - (searchBufferStart + index);
         length = potentialMatch.length;
-        break; 
+        break;
       }
     }
 
     if (length > 0) {
-      compressLzString.push([offset, length, data[codingPosition + length] || '']);
+      compressLzString.push([
+        offset,
+        length,
+        data[codingPosition + length] || "",
+      ]);
       codingPosition += length + 1;
     } else {
       compressLzString.push([0, 0, data[codingPosition]]);
@@ -37,4 +44,21 @@ function compress(data) {
   return compressLzString;
 }
 
-console.log(compress("abab"));
+
+function decompress(data) {
+  let decompressed = [];
+
+  for (const [distance, length, nextChar] of data) {
+    if (length === 0) {
+      decompressed.push(nextChar);
+    } else {
+      const start = decompressed.length - distance;
+      for (let i = 0; i < length; i++) {
+        decompressed.push(decompressed[start + i]);
+      }
+      decompressed.push(nextChar);
+    }
+  }
+
+  return decompressed.toString();
+}
